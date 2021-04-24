ee7e79e5d2b5e62ff1729bd37c9022f9e2886270
jsGolden <foolycoolyvip@gmail.com>
2021-04-24 13:02:50 -0300

    API transferida para URL Pública

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.1.105:3333',
+    baseURL: 'https://my-json-server.typicode.com/jsGolden/plant-manager',
 });
 
 export default api;
